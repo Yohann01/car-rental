7986a6bd56a957acb7cadf73ec882834d3fb3177
Yohann01 <82199055+Yohann01@users.noreply.github.com>
2024-05-03 22:10:38 +0800

    add: responsive about page

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -8,4 +8,10 @@ exports.getSignIn = async (req, res) => {
 exports.getSignUp = async (req, res) => {
     res.render('signup');
 }
+exports.getAbout= async (req, res) => {
+    res.render('about', {layout: './layouts/about-contact-main'});
+}
+exports.getContact = async (req, res) => {
+    res.render('contact', {layout: './layouts/about-contact-main'});
+}
 
--- routes/homepage.js
+++ routes/homepage.js
@@ -6,6 +6,8 @@ const homepageController = require('../controllers/homepageController');
 router.get('/' , homepageController.getHomepage);
 router.get('/login', homepageController.getSignIn);
 router.get('/signup', homepageController.getSignUp);
+router.get('/about', homepageController.getAbout);
+router.get('/contact', homepageController.getContact);
 
 
 
